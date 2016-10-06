cc42ce2b0aa71ecdd83a44eeefc31d8a7e94dc02
Eugene Pirogov <iamexile@gmail.com>
2016-10-06 16:34:14 +0300

    Extract user routes

--- lib/integration_layer/router.ex
+++ lib/integration_layer/router.ex
@@ -1,37 +1,13 @@
 defmodule IntegrationLayer.Router do
   use Plug.Router
 
-  plug :match
-
   plug IntegrationLayer.Config
   plug IntegrationLayer.Auth
   plug IntegrationLayer.ACL
 
+  forward "/configs", to: IntegrationLayer.Routes.Configs
+  forward "/user", to: IntegrationLayer.Routes.Users
+
+  plug :match
   plug :dispatch
-
-
-  forward "/config", to: IntegrationLayer.Routes.Config
-
-  post "/create_user" do
-    fake_response = ~s(Successfully created user using internal "#{conn.assigns.config.upstream_path}" service.)
-
-    send_resp(conn, 200, fake_response)
-  end
-
-  post "/create_user_async" do
-    conn  = fetch_query_params(conn)
-
-    url_from_config = conn.assigns.config.upstream_path
-    callback_url = conn.params["callback_url"] # "http://requestb.in/s7wnp7s7"
-
-    Task.async fn ->
-      # add some headers when sending a response to a backend
-      response = HTTPoison.get!(url_from_config, conn.req_headers).body
-
-      # add some headers when sending a response back to original requester
-      HTTPoison.post!(callback_url, response)
-    end
-
-    send_resp(conn, 200, "Successfully scheduled creating a user! Once we're done, we'll send a reply to #{callback_url}")
-  end
 end
---
+++ lib/integration_layer/routes/users.ex
@@ -0,0 +1,31 @@
+defmodule IntegrationLayer.Routes.Users do
+  use Plug.Router
+
+  plug :match
+  plug :dispatch
+
+  post "/create" do
+    fake_response = ~s(Successfully created user using internal "#{conn.assigns.config.upstream_path}" service.)
+
+    send_resp(conn, 200, fake_response)
+  end
+
+  post "/create_async" do
+    conn  = fetch_query_params(conn)
+
+    url_from_config = conn.assigns.config.upstream_path
+    callback_url = conn.params["callback_url"] # "http://requestb.in/s7wnp7s7"
+
+    Task.async fn ->
+      # add some headers when sending a response to a backend
+      
+      # simulate some work sent to upstream
+      response = HTTPoison.get!(url_from_config, conn.req_headers).body
+
+      # add some headers when sending a response back to original requester
+      HTTPoison.post!(callback_url, response)
+    end
+
+    send_resp(conn, 200, "Successfully scheduled creating a user! Once we're done, we'll send a reply to #{callback_url}")
+  end
+end
