7987f731efe561cd363ec55e62493111bac10240
Eugene Pirogov <iamexile@gmail.com>
2016-10-06 16:35:38 +0300

    Subtle code styling

--- lib/integration_layer/routes/configs.ex
+++ lib/integration_layer/routes/configs.ex
@@ -5,8 +5,8 @@ defmodule IntegrationLayer.Routes.Configs do
   plug :dispatch
 
   get "/" do
-    conn  = fetch_query_params(conn)
-    path   = conn.params["path"]
+    conn = fetch_query_params(conn)
+    path = conn.params["path"]
 
     [{ _key, value }] = :ets.lookup(:my_configs, path)
 
@@ -15,13 +15,12 @@ defmodule IntegrationLayer.Routes.Configs do
 
   put "/" do
     conn  = fetch_query_params(conn)
-    path  = conn.params["path"]
 
+    path  = conn.params["path"]
     key   = conn.params["key"]
     value = conn.params["value"]
 
     [{ _key, existing_config }] = :ets.lookup(:my_configs, path)
-    |> IO.inspect
 
     new_config = Map.put(existing_config, String.to_atom(key), value)
     :ets.insert(:my_configs, { path, new_config })
